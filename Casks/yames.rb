e9a85745836b8d5893b2e151414f10a4b781d8a0
Alberto Delgado <github-actions[bot]@users.noreply.github.com>
2026-05-04 10:15:22 -0700

    chore: update yames cask to v0.5.0

--- Casks/yames.rb
+++ Casks/yames.rb
@@ -1,14 +1,14 @@
 cask "yames" do
-  version "0.4.2"
+  version "0.5.0"
 
   on_arm do
     url "https://github.com/turutupa/yames/releases/download/v#{version}/Yames_#{version}_aarch64.dmg"
-    sha256 :no_check
+    sha256 "f74820625933d504663ce5888a3f4d2a9a42687cdfecd3f08d2f2024731a3440"
   end
 
   on_intel do
     url "https://github.com/turutupa/yames/releases/download/v#{version}/Yames_#{version}_x64.dmg"
-    sha256 :no_check
+    sha256 "8fb55e133c0acd9b68b9d617ecf3ab7155df3dee2dcc0587c8da5ccb0e8a65d0"
   end
 
   name "Yames"
@@ -21,12 +21,6 @@ cask "yames" do
   end
 
   app "Yames.app"
-
-  postflight do
-    system_command "/usr/bin/xattr",
-                   args: ["-cr", "#{appdir}/Yames.app"],
-                   sudo: false
-  end
 
   zap trash: [
     "~/Library/Application Support/com.yames.metronome",
